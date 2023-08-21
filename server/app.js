06c3c801d7d2bb4359aff0d172581980e7812a4c
Stanley.js <jamais.stanley@gmail.com>
2023-08-21 08:17:07 +0200

    new endpoints testable

--- server/app.js
+++ server/app.js
@@ -5,11 +5,19 @@ const port = 3000;
 const app = express();
 
 const contestsRoute = require('./routes/contests');
+const payRoute = require('./routes/payment');
+const blogRoute = require('./routes/blog');
+const messengerRoute = require('./routes/messenger');
+const metricsRoute = require('./routes/metrics');
 
 
 app.use(express.json());
 
 app.use('/contest',contestsRoute);
+app.use('/pay',payRoute);
+app.use('/blog',blogRoute);
+app.use('/messenger',messengerRoute);
+app.use('/metrics',metricsRoute);
 
 app.listen(port, () => {
   console.log(`Server listen on port : ${port}`);
