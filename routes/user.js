dad6fb7441897b33e66cfb10cbaf6e9a316e5495
Stanley.js <jamais.stanley@gmail.com>
2023-09-04 15:11:57 +0200

    Adding utils and clear code for better view

---
+++ routes/user.js
@@ -0,0 +1,51 @@
+const {
+    express,
+    mysql,
+    router,
+    bodyParser,
+    moment,
+    errorLog,
+    db,
+    urlencodedParser,
+    date
+  } = require('../controllers/express-utils.js');
+
+  // Route pour récupérer les utilisateurs par id
+
+router.get('/:id', (req, res) => {
+    db.query('SELECT * FROM users WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+// Route pour récupérer tous les utilisateurs
+router.get('/', (req, res) => {
+    db.query('SELECT * FROM users', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+
+// Route pour créer un utilisateur
+
+router.post('/',urlencodedParser, (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const date = req.body.date;
+    db.query('INSERT INTO users (name, email, password, date) VALUES (?,?,?,?)', [name, email, password, date], (err, result) => {
+        if (err) {
+            errorLog(err,date);
+            res.status(500).send('Error trying to create user');
+        }
+        res.status(201).send(result);
+    });
+});
+
+// Route pour modifier un utilisateur
